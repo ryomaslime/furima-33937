bffb677e36c088314f997b7d8811ed820f028ded
ryomaslime <77054799+ryomaslime@users.noreply.github.com>
2021-02-10 16:39:27 +0900

    modification1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+before_action :item_name, only: [:show, :edit, :update, :move_to_index]
 before_action :move_to_index, only: [:edit, :update]
 
   def index
@@ -20,15 +21,12 @@ before_action :move_to_index, only: [:edit, :update]
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -43,9 +41,12 @@ before_action :move_to_index, only: [:edit, :update]
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     if current_user.id != @item.user.id
       redirect_to root_path
     end
+  end
+
+  def item_name
+    @item = Item.find(params[:id])
   end
 end
